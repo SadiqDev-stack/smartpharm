8fdae499f07b3580d8bd10ca03f554e68cfed16c
SadiqDev-stack <sadiqmuh1321@gmail.com>
2026-07-06 02:36:51 -0700

    change frontend to mobile app

--- frontend/src/hooks/useStorage.js
+++
@@ -1,8 +0,0 @@
-import { StorageContext } from "../context/StorageContext";
-import { useContext } from "react";
-
-const useStorage = () => {
-  return useContext(StorageContext);
-};
-
-export default useStorage;
